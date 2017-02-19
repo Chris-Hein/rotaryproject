204d372b698ba46b1023e66fda4e6bf56261ffac
Chris <rhodes51166db@yahoo.ca>
2017-02-18 21:56:02 -0400

    admin updated invoice page added
    admin updated to include master page, adInvoice page added. adInvoice
    handles automated invoices and sends them out via selected email, also
    handles automated email messages that the admin can write themselves
    
    These updates should take care of the automated invoice (major priority)
    and automatic invoice (minor priority) from sprint 3, as we're handling
    the financial and business related aspects at this point prior to the
    photographer being involved (as invoices go out in the invoice page and
    photographer is assigned in the admin page)
    
    Note: we'll have to go through and ensure everything is committing
    into/retrieving the right parts of the database once all the components
    of the site are finished, I suspect they're not atm but that'll be fixed
    during initial internal testing

---
+++ Files/App_Code/Review.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Sql;
+using System.Data;
+using MySql.Data.MySqlClient;
+
+public class Review {
+    // Connection objects
+    MySqlConnection dbConnection;
+    MySqlCommand dbCommand;
+    string sqlString;
+
+    public Review() {
+        // Initialization
+    }
+
+    public string getAdSize(string email) {
+        try {
+            dbConnection = new MySqlConnection("Database=rotaryyearbook;Data Source=localhost;User Id=useraccount;Password=userpassword");
+            dbConnection.Open();
+            sqlString = "SELECT ad_size FROM adawaitingapproval WHERE business_email = '" + email + "'";
+            dbCommand = new MySqlCommand(sqlString, dbConnection);
+            // Uses executescalar because there is only one thing that needs to be returned
+            return dbCommand.ExecuteScalar().ToString();
+        } finally {
+            dbConnection.Close();
+        }
+    }
+
+    public string getFirstName(string email) {
+        try {
+            dbConnection = new MySqlConnection("Database=rotaryyearbook;Data Source=localhost;User Id=useraccount;Password=userpassword");
+            dbConnection.Open();
+            sqlString = "SELECT first_name FROM adawaitingapproval WHERE business_email = '" + email + "'";
+            dbCommand = new MySqlCommand(sqlString, dbConnection);
+            // Uses executescalar because there is only one thing that needs to be returned
+            return dbCommand.ExecuteScalar().ToString();
+        } finally {
+            dbConnection.Close();
+        }
+    }
+
+    public string getLastName(string email) {
+        try {
+            dbConnection = new MySqlConnection("Database=rotaryyearbook;Data Source=localhost;User Id=useraccount;Password=userpassword");
+            dbConnection.Open();
+            sqlString = "SELECT last_name FROM adawaitingapproval WHERE business_email = '" + email + "'";
+            dbCommand = new MySqlCommand(sqlString, dbConnection);
+            // Uses executescalar because there is only one thing that needs to be returned
+            return dbCommand.ExecuteScalar().ToString();
+        } finally {
+            dbConnection.Close();
+        }
+    }
+
+    public string getBusinessName(string email) {
+        try {
+            dbConnection = new MySqlConnection("Database=rotaryyearbook;Data Source=localhost;User Id=useraccount;Password=userpassword");
+            dbConnection.Open();
+            sqlString = "SELECT business_name FROM adawaitingapproval WHERE business_email = '" + email + "'";
+            dbCommand = new MySqlCommand(sqlString, dbConnection);
+            // Uses executescalar because there is only one thing that needs to be returned
+            return dbCommand.ExecuteScalar().ToString();
+        } finally {
+            dbConnection.Close();
+        }
+    }
+
+    public string getEmail(string email) {
+        try {
+            dbConnection = new MySqlConnection("Database=rotaryyearbook;Data Source=localhost;User Id=useraccount;Password=userpassword");
+            dbConnection.Open();
+            sqlString = "SELECT business_email FROM adawaitingapproval WHERE business_email = '" + email + "'";
+            dbCommand = new MySqlCommand(sqlString, dbConnection);
+            // Uses executescalar because there is only one thing that needs to be returned
+            return dbCommand.ExecuteScalar().ToString();
+        } finally {
+            dbConnection.Close();
+        }
+    }
+}
