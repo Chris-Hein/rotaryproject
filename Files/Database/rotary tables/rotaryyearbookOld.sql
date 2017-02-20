7074bc040bfe5085d6630c5f17a5c2ac290f6aba
Chris <rhodes51166db@yahoo.ca>
2017-02-19 21:55:18 -0400

    user business records page and updated db
    Added the business records page as per sprint 3 and an updated version
    of the database as i've been using it

--- Files/Database/rotary tables/rotaryyearbookOld.sql
+++ Files/Database/rotary tables/rotaryyearbookOld.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 20, 2017 at 02:49 AM
+-- Generation Time: Jan 30, 2017 at 06:08 AM
 -- Server version: 10.1.10-MariaDB
 -- PHP Version: 7.0.4
 
@@ -19,35 +19,6 @@ SET time_zone = "+00:00";
 --
 -- Database: `rotaryyearbook`
 --
-
--- --------------------------------------------------------
-
---
--- Table structure for table `adawaitingapproval`
---
-
-CREATE TABLE `adawaitingapproval` (
-  `id` int(255) NOT NULL,
-  `image` varchar(50) NOT NULL,
-  `caption` varchar(200) NOT NULL,
-  `business_name` varchar(100) NOT NULL,
-  `first_name` varchar(50) NOT NULL,
-  `last_name` varchar(50) NOT NULL,
-  `phone` varchar(15) NOT NULL,
-  `business_email` varchar(50) NOT NULL,
-  `business_address` varchar(200) NOT NULL,
-  `ad_size` varchar(15) NOT NULL,
-  `approval_status` varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `adawaitingapproval`
---
-
-INSERT INTO `adawaitingapproval` (`id`, `image`, `caption`, `business_name`, `first_name`, `last_name`, `phone`, `business_email`, `business_address`, `ad_size`, `approval_status`) VALUES
-(1, 'testimage1.jpg', 'caption for test image 1', 'business name test image 1', 'test image', 'one', '902-555-1111', 'email@testimage1.com', '123 test image 1 street', '1/3 Page', 'Not Approved'),
-(2, 'testimage2.jpg', 'caption for test image 2', 'business name test image 2', 'test image', 'two', '902-555-2222', 'email@testimage2.com', '123 test image 2 street', '1/2 Page', 'Not Approved'),
-(3, 'testimage3.jpg', 'caption for test image 3', 'business name for test image 3', 'test image', 'three', '902-555-3333', 'email@testimage3.com', '123 test image 3 street', 'Full Page', 'Not Approved');
 
 -- --------------------------------------------------------
 
@@ -75,53 +46,10 @@ CREATE TABLE `admin` (
   `assigned_photographer_name` varchar(50) NOT NULL,
   `assigned_photographer_phone` varchar(15) NOT NULL,
   `assigned_photographer_email` varchar(50) NOT NULL,
-  `assigned_business_name` varchar(50) NOT NULL,
-  `assigned_business_phone` varchar(15) NOT NULL,
-  `assigned_business_email` varchar(50) NOT NULL
+  `assigned_sponsor_name` varchar(50) NOT NULL,
+  `assigned_sponsor_phone` varchar(15) NOT NULL,
+  `assigned_sponsor_email` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `admin`
---
-
-INSERT INTO `admin` (`id`, `business_name`, `ad_size`, `pay_type`, `business_email`, `contacted`, `ordered`, `business_phone`, `business_address`, `haspaid`, `ad_approved`, `first_name`, `middle_name`, `last_name`, `image`, `image_caption`, `assigned_photographer_name`, `assigned_photographer_phone`, `assigned_photographer_email`, `assigned_business_name`, `assigned_business_phone`, `assigned_business_email`) VALUES
-(1, 'Coffee Inc', 'full page', 'Paypal', 'myemail@email.com', 'No', 'No', '902-555-5555', '123 fake street, chicago', 'No', 'No', 'John', 'J', 'Smith', 'testimage.jpg', 'this is my caption', 'Jane Smith', '902-555-6666', 'photographer@email.com', 'assigned business', '902-555-5555', 'mail@mail.com'),
-(2, 'Microsoft', '1/3 Page', 'Cash', 'email@email.com', 'Yes', 'No', '902-555-5555', '12345 hamster drive', 'No', 'No', 'Bill', 'J', 'Gates', 'testimage.jpg', 'this is a caption', 'Joe McTest', '902-555-5555', 'mail@mail.com', 'business name', '902-555-5555', 'mail@mail.com'),
-(3, 'Cryptic Studios', '1/3 Page', 'Paypal', 'email@email.com', 'No', 'No', '902-555-5555', '12345 hamster drive', 'No', 'No', 'Jack', 'J', 'Smith', 'testimage.jpg', 'this is a caption', 'Joe McTest', '902-555-5555', 'mail@mail.com', 'business name', '902-555-5555', 'mail@mail.com'),
-(4, 'Sheepdog Inc', '1/6 Page', 'Cash', 'mymail@email.com', 'No', 'No', '902-555-5555', '123 Fake Ave', 'No', 'No', 'Jackie', 'J', 'Faker', 'testimage.jpg', 'caption', 'Jay McSheepdog', '902-555-5555', 'mail@mail.com', 'business name', '902-555-5555', 'mail@mail.com'),
-(5, 'Smirnoff', '1/6 Page', 'Cash', 'mymail@email.com', 'No', 'No', '902-555-5555', '123 Fake Ave', 'No', 'No', 'Jackie', 'J', 'Faker', 'testimage.jpg', 'caption', 'Jay McFake', '902-555-5555', 'mail@mail.com', 'business name', '902-555-5555', 'mail@mail.com'),
-(6, 'NSCC', '1/2 Page', 'Paypal', 'mymail@mail.com', 'No', 'No', '902-555-5555', '123 Maple Street', 'No', 'No', 'Jenny', 'J', 'McFaker', 'testimage.jpg', 'image caption', 'Jay McSheepdog', '902-555-5555', 'photo@mail.com', 'Sheepdog Inc', '902-555-5555', 'email@mymail.com'),
-(7, 'Acadia', '1/2 Page', 'Paypal', 'mymail@mail.com', 'No', 'No', '902-555-5555', '123 Maple Street', 'No', 'No', 'Jenny', 'J', 'McFaker', 'testimage.jpg', 'image caption', 'Jay McSheepdog', '902-555-5555', 'photo@mail.com', 'Sheepdog Inc', '902-555-5555', 'email@mymail.com');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `businessrecords`
---
-
-CREATE TABLE `businessrecords` (
-  `id` int(255) NOT NULL,
-  `business_name` varchar(50) NOT NULL,
-  `first_name` varchar(50) NOT NULL,
-  `last_name` varchar(50) NOT NULL,
-  `ad_size` varchar(15) NOT NULL,
-  `ad_price` varchar(10) NOT NULL,
-  `date` varchar(15) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `businessrecords`
---
-
-INSERT INTO `businessrecords` (`id`, `business_name`, `first_name`, `last_name`, `ad_size`, `ad_price`, `date`) VALUES
-(1, 'Sheepdog Inc', 'Mister', 'Sheepdog', '1/3 Page', '1400', '05/05/99'),
-(2, 'Sheepdog Inc', 'Mrs', 'Sheepdog', 'Full Page', '3500', '02/03/97'),
-(3, 'Sheepdog Inc', 'Johnny', 'McSheepdog', '1/6 Page', '800', '01/01/94'),
-(4, 'Sheepdog Inc', 'Jane', 'McSheepdog', 'Full Page', '3500', '07/03/92'),
-(5, 'Sheepdog Inc', 'Sean', 'McSheepdog', '1/3 Page', '1200', '02/02/95'),
-(6, 'Sheepdog Inc', 'Joe', 'Sheepy', '1/6 Page', '1400', '08/21/96'),
-(7, 'username', 'Joe', 'Tester', '2/3 Page', '2000', '05/16/00'),
-(8, 'username', 'Jane', 'Tester', 'Full Page', '2500', '04/19/02');
 
 -- --------------------------------------------------------
 
@@ -235,21 +163,9 @@ CREATE TABLE `sponsor` (
 --
 
 --
--- Indexes for table `adawaitingapproval`
---
-ALTER TABLE `adawaitingapproval`
-  ADD PRIMARY KEY (`id`);
-
---
 -- Indexes for table `admin`
 --
 ALTER TABLE `admin`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `businessrecords`
---
-ALTER TABLE `businessrecords`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -293,20 +209,10 @@ ALTER TABLE `sponsor`
 --
 
 --
--- AUTO_INCREMENT for table `adawaitingapproval`
---
-ALTER TABLE `adawaitingapproval`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
---
 -- AUTO_INCREMENT for table `admin`
 --
 ALTER TABLE `admin`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
---
--- AUTO_INCREMENT for table `businessrecords`
---
-ALTER TABLE `businessrecords`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `image`
 --
