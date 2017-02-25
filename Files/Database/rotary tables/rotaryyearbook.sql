4f4992d61a7028a703ab67109771b3c5aa1e3b9a
Matthew Hutchinson <Zeke725@users.noreply.github.com>
2017-02-24 20:19:30 -0400

    Added login info for testing

--- Files/Database/rotary tables/rotaryyearbook.sql
+++ Files/Database/rotary tables/rotaryyearbook.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.1
--- http://www.phpmyadmin.net
+-- version 4.6.6
+-- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Feb 20, 2017 at 02:49 AM
--- Server version: 10.1.10-MariaDB
--- PHP Version: 7.0.4
+-- Host: localhost
+-- Generation Time: Feb 24, 2017 at 08:18 PM
+-- Server version: 5.7.17-log
+-- PHP Version: 7.1.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -167,6 +167,17 @@ CREATE TABLE `login` (
   `password` varchar(20) NOT NULL,
   `usertype` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `login`
+--
+
+INSERT INTO `login` (`id`, `username`, `password`, `usertype`) VALUES
+(0, 'rulescard', 'juicebox31', 'master'),
+(1, 'aceofclubs', 'tastydish12', 'admin'),
+(2, 'jackofclubs', 'pointandclick24', 'photographer'),
+(3, 'twoofclubs', 'thimble86', 'solicitor'),
+(4, 'BCISBC', 'business01', 'BCISBC');
 
 -- --------------------------------------------------------
 
