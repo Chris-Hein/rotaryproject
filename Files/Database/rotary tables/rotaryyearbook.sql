01a7d5777f66138a6f9e7373fc81d3b2f85f07e5
Matthew Hutchinson <Zeke725@users.noreply.github.com>
2017-03-02 18:45:13 -0400

    added data to new fields

--- Files/Database/rotary tables/rotaryyearbook.sql
+++ Files/Database/rotary tables/rotaryyearbook.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Mar 02, 2017 at 04:42 PM
+-- Generation Time: Mar 02, 2017 at 06:44 PM
 -- Server version: 5.7.17-log
 -- PHP Version: 7.1.2
 
@@ -30,7 +30,7 @@ CREATE TABLE `addata` (
   `id` int(255) NOT NULL,
   `approved` varchar(3) NOT NULL DEFAULT 'no',
   `sponsorName` text NOT NULL,
-  `sponsorAddress` text,
+  `captionAddress` text,
   `captionPhone` varchar(15) DEFAULT NULL,
   `captionEmail` varchar(100) DEFAULT NULL,
   `captionFront` text,
@@ -44,8 +44,8 @@ CREATE TABLE `addata` (
 -- Dumping data for table `addata`
 --
 
-INSERT INTO `addata` (`id`, `approved`, `sponsorName`, `sponsorAddress`, `captionPhone`, `captionEmail`, `captionFront`, `captionMiddle`, `captionBack`, `image`, `adNotes`) VALUES
-(1, 'yes', 'Coffee Express', NULL, NULL, NULL, 'It\'s coffee!!', NULL, NULL, 'coffee1.jpg', NULL),
+INSERT INTO `addata` (`id`, `approved`, `sponsorName`, `captionAddress`, `captionPhone`, `captionEmail`, `captionFront`, `captionMiddle`, `captionBack`, `image`, `adNotes`) VALUES
+(1, 'no', 'Coffee Express', NULL, NULL, NULL, 'It\'s coffee!!', NULL, NULL, 'coffee1.jpg', 'Beans do not fill the screen'),
 (2, 'yes', 'Sheepdog Inc.', NULL, NULL, NULL, NULL, 'Ralph', NULL, 'sheepdog.jpg', NULL),
 (3, 'no', 'Matt\'s Bistro', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
 (4, 'no', 'Patches and Stitches', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
@@ -118,18 +118,18 @@ CREATE TABLE `mainrecords` (
 
 INSERT INTO `mainrecords` (`id`, `sponsorName`, `sponsorAddress`, `sponsorContact`, `sponsorPhone`, `sponsorEmail`, `solicitor`, `ordered`, `adSize`, `price`, `paid`, `datePaid`, `photographer`, `sponsorNotes`) VALUES
 (1, 'Asbestos Miner\'s Guild', '43557 Underground Ave', 'Boris Stone', '9025550001', 'mail@asbestosrocks.ca', 'twoofclubs', 'needs visit', NULL, NULL, NULL, NULL, NULL, NULL),
-(2, 'Matt\'s Bistro', '47 Meat St', 'Matthew Cutter', '9025551234', 'mail@mattsmeat.ca', 'threeofclubs', 'yes', 'full page', 1725, 'no', NULL, NULL, NULL),
+(2, 'Matt\'s Bistro', '47 Meat St', 'Matthew Cutter', '9025551234', 'mail@mattsmeat.ca', 'threeofclubs', 'yes', 'full page', 1725, 'no', NULL, NULL, 'Don\'t ask Matt about his meat.'),
 (3, 'Roadkill Animal Shelter', '127 Taxidermy Ln', 'Steve Skinnington', '902-555-4545', 'mail@rks.ca', 'fourofclubs', 'needs visit', NULL, NULL, NULL, NULL, NULL, NULL),
-(4, 'The Fun Police', '100 Jailhouse Crt', 'Sgt. Hardass', '902-555-5845', 'mail@funpolice.ca', 'twoofclubs', 'no', NULL, NULL, NULL, NULL, NULL, NULL),
+(4, 'The Fun Police', '100 Jailhouse Crt', 'Sgt. Hardass', '902-555-5845', 'mail@funpolice.ca', 'twoofclubs', 'pending', NULL, NULL, NULL, NULL, NULL, 'Rotarian may be subjected to a humiliating search.'),
 (5, 'The Sandwich Master', '24 Baguette Bvd', 'Gerald Crunch', '902-555-3287', 'mail@sandwichmaster.ca', 'threeofclubs', 'yes', 'third of a page', 690, 'credit', '2017-02-27', 'jackofclubs', NULL),
-(6, 'Coffee Express', '1 Zip Ln', 'Danny Swift', '902-555-9666', 'mail@coffeeexpress.ca', 'twoofclubs', 'yes', 'sixth of a page', 415, 'cash', '2017-02-27', 'jackofclubs', NULL),
+(6, 'Coffee Express', '1 Zip Ln', 'Danny Swift', '902-555-9666', 'mail@coffeeexpress.ca', 'twoofclubs', 'yes', 'sixth of a page', 415, 'cash', '2017-02-27', 'jackofclubs', 'Good coffee'),
 (7, 'Patches and Stitches', '5620 Fabric St', 'Cindy Weaver', '902-555-3456', 'mail@patchesstitches.ca', 'fourofclubs', 'yes', 'sixth of a page', 415, 'paypal', '2017-02-06', 'jackofclubs', NULL),
 (8, 'Cryptic Studios', '2112 Cyber St', 'Ctoggha', '902-555-0909', 'mail@mythos.ca', 'threeofclubs', 'yes', 'full page', 1725, 'no', NULL, 'jackofclubs', NULL),
 (9, 'Sheepdog Inc', '488 Mellowfield Dr', 'Ralph Sheepdog', '902-555-3640', NULL, 'twoofclubs', 'yes', 'two thirds of a page', 1150, 'cash', '2017-02-01', 'jackofclubs', NULL),
 (10, 'Beefy\'s Burgers', '86 Hamburger Ave', 'Jane Cowmangler', '902-555-2333', NULL, 'threeofclubs', 'yes', 'two thirds of a page', 1150, 'credit', '2017-01-04', 'jackofclubs', NULL),
 (11, 'The Chronically Horrid', '5555 News St', 'Bill Paper', '902-555-5445', 'mail@anewspaper.ca', 'fourofclubs', 'no', NULL, NULL, NULL, NULL, NULL, NULL),
 (12, 'Gas and Pass', '98 Diesel St', 'Gus Carson', '902-555-8888', 'mail@passgas.ca', 'fourofclubs', 'yes', 'third of a page', 690, 'yes', '2017-02-07', 'jackofclubs', NULL),
-(13, 'Hugh Bigshot', '100 Fancy Ave', 'Miss Grovel, secretary', '902-555-1111', 'press@hughbigshot.ca', 'threeofclubs', 'yes', 'full page', 1500, 'cash', '2017-02-01', 'jackofclubs', NULL);
+(13, 'Hugh Bigshot', '100 Fancy Ave', 'Miss Grovel, secretary', '902-555-1111', 'press@hughbigshot.ca', 'threeofclubs', 'yes', 'full page', 1500, 'cash', '2017-02-01', 'jackofclubs', 'Will be difficult to fit ego into picture');
 
 --
 -- Indexes for dumped tables
